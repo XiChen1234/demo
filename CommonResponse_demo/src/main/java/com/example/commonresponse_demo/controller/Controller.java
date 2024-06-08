5f87282563f09dbce8f4f1b9e985b3bc6de12adf
Xi_Chen <xichen888.private@gmail.com>
2024-06-09 01:42:25 +0800

    fix: delete service and dao

--- CommonResponse_demo/src/main/java/com/example/commonresponse_demo/controller/Controller.java
+++ CommonResponse_demo/src/main/java/com/example/commonresponse_demo/controller/Controller.java
@@ -1,22 +1,17 @@
 package com.example.commonresponse_demo.controller;
 
 import com.example.commonresponse_demo.common.CommonResponse;
-import com.example.commonresponse_demo.service.MyService;
-import com.example.commonresponse_demo.service.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.util.List;
+import java.util.Objects;
 
 /**
  * @Description api接口层
  */
 @RestController
 public class Controller {
-    @Resource
-    private MyService myService;
 
     @GetMapping("/test")
     public String test() {
@@ -25,6 +20,7 @@ public class Controller {
 
     /**
      * 三种通用响应类型
+     *
      * @param type 类型
      *             success：成功200
      *             warning：警告400
@@ -32,7 +28,19 @@ public class Controller {
      * @return 通用响应类
      */
     @GetMapping("/common/{type}")
-    public CommonResponse<List<Student>> getList(@PathVariable String type) {
-        return myService.getStudentList(type);
+    public CommonResponse<String> response(@PathVariable String type) {
+        if (Objects.equals(type, "success")) {
+            return CommonResponse.creatForSuccessData("my data is here");
+        }
+
+        if (Objects.equals(type, "warning")) {
+            return CommonResponse.creatForWarning();
+        }
+
+        if (Objects.equals(type, "error")) {
+            return CommonResponse.creatForErrorMessage("服务器错误");
+        }
+
+        return CommonResponse.creatForWarningMessage("参数判断错误");
     }
 }
