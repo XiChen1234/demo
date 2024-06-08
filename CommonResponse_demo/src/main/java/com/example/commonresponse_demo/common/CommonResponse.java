162acc0f39a8ead4f1410628acdfe94b23159379
Xi_Chen <xichen888.private@gmail.com>
2024-06-09 01:35:35 +0800

    fix: change common response structure
    修改通用响应类的基本结构

--- CommonResponse_demo/src/main/java/com/example/commonresponse_demo/common/CommonResponse.java
+++ CommonResponse_demo/src/main/java/com/example/commonresponse_demo/common/CommonResponse.java
@@ -3,8 +3,6 @@ package com.example.commonresponse_demo.common;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
-import java.util.Objects;
-
 /**
  * @Description 通用响应类结构
  */
@@ -12,93 +10,101 @@ import java.util.Objects;
 @JsonInclude(JsonInclude.Include.NON_NULL) // 空数据不包含
 public class CommonResponse<T> {
     private final Integer code;
-    private final String message;
-    private final T data;
+    private String message;
+    private T data;
 
     // 私有构造方法
+    private CommonResponse(Integer code) {
+        this.code = code;
+    }
+
+    private CommonResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
     private CommonResponse(Integer code, String message, T data) {
         this.code = code;
         this.message = message;
         this.data = data;
     }
 
-    // --- 成功响应 --- //
 
     /**
-     * 创建成功响应
+     * 创建成功响应方法
      *
-     * @return 带code的成功响应
+     * @return 成功响应
      */
     public static <T> CommonResponse<T> creatForSuccess() {
-        return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMessage(), null);
+        return new CommonResponse<>(ResponseCode.SUCCESS.getCode());
     }
 
     /**
-     * 创建成功响应 message
+     * 创建成功响应方法
      *
-     * @return 带code message的成功响应
+     * @param message 成功自定义信息
+     * @return 成功响应
      */
     public static <T> CommonResponse<T> creatForSuccessMessage(String message) {
-        return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), message, null);
+        return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), message);
     }
 
     /**
-     * 创建成功响应 data
+     * 创建成功响应方法
      *
-     * @return 带code data的成功响应
+     * @param data 响应数据
+     * @return 成功响应
      */
     public static <T> CommonResponse<T> creatForSuccessData(T data) {
         return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMessage(), data);
     }
 
     /**
-     * 创建成功响应 message data
+     * 创建成功响应方法
      *
-     * @return 带code message data的成功响应
+     * @param message 成功自定义信息
+     * @param data    响应数据
+     * @return 成功响应
      */
     public static <T> CommonResponse<T> creatForSuccessMessageData(String message, T data) {
         return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), message, data);
     }
 
-    // --- 警告响应 --- //
-
     /**
-     * 创建警告响应
+     * 创建警告响应方法
      *
-     * @return 警告响应
+     * @return 异常响应
      */
     public static <T> CommonResponse<T> creatForWarning() {
-        return new CommonResponse<>(ResponseCode.WARNING.getCode(), ResponseCode.ERROR.getMessage(), null);
+        return new CommonResponse<>(ResponseCode.WARNING.getCode());
     }
 
     /**
-     * 创建警告响应
+     * 创建警告响应方法
      *
-     * @param message 自定义信息
-     * @return 警告响应
+     * @param message 警告自定义信息
+     * @return 异常响应
      */
     public static <T> CommonResponse<T> creatForWarningMessage(String message) {
-        return new CommonResponse<>(ResponseCode.WARNING.getCode(), message, null);
+        return new CommonResponse<>(ResponseCode.WARNING.getCode(), message);
     }
 
-    // ---  异常响应 --- //
-
     /**
-     * 创建错误响应
+     * 创建错误响应方法
      *
-     * @return 警告响应
+     * @return 错误响应
      */
     public static <T> CommonResponse<T> creatForError() {
-        return new CommonResponse<>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getMessage(), null);
+        return new CommonResponse<>(ResponseCode.ERROR.getCode());
     }
 
     /**
-     * 创建错误响应
+     * 创建错误响应方法
      *
-     * @param message 自定义信息
-     * @return 警告响应
+     * @param message 错误自定义信息
+     * @return 错误响应
      */
     public static <T> CommonResponse<T> creatForErrorMessage(String message) {
-        return new CommonResponse<>(ResponseCode.ERROR.getCode(), message, null);
+        return new CommonResponse<>(ResponseCode.ERROR.getCode(), message);
     }
 }
